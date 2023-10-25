04f0a5470dedd8d568a955ac37f8c95104eb5363
Renato José da Silva <renatojsilvas@gmail.com>
2023-10-25 15:10:11 -0300

    Adiciona politica de CORS

--- Program.cs
+++ Program.cs
@@ -13,6 +13,11 @@ namespace AgendaDeAulas
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            builder.Services.AddCors(policyBuilder =>
+                      policyBuilder.AddDefaultPolicy(policy =>
+                        policy.WithOrigins("*").AllowAnyHeader().AllowAnyHeader())
+            );
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -23,7 +28,7 @@ namespace AgendaDeAulas
             }
 
             app.UseAuthorization();
-
+            app.UseCors();
 
             app.MapControllers();
 
